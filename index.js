2830113c0822df16a1590f6cb84031a6a2f80bc8
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-04-09 12:21:09 +0300

    commit tree 65737975a0475bd9cfa729287cbf8baf00826519

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+countDown(10);
+
+
+
+
+function writeCards(names, eventName) {
+    const result = [];
+    for (let i = 0; i < names.length; i++)
+        result.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    return result;
+}
+
+function countDown(num) {
+    while (num >= 0)
+        console.log(num--);
+}
